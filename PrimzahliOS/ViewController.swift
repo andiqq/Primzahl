d1fc40335eca8f6934f903a3afd5d4966d40fafb
Dr. Andreas Plagens <andreas@plagens.eu>
2020-04-28 21:34:18 +0200

    Initial Commit

---
+++ PrimzahliOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrimzahliOS
+//
+//  Created by Dr. Andreas Plagens on 28.04.20.
+//  Copyright © 2020 Dr. Andreas Plagens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
