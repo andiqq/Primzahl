fddc641649ba30abf5685753a78c5b9b08470fba
Dr. Andreas Plagens <andreas@plagens.eu>
2025-04-04 20:19:13 +0200

    removed the function call

--- Primzahl5/main.swift
+++ Primzahl5/main.swift
@@ -1,11 +1,34 @@
-//
-//  main.swift
-//  Primzahl5
-//
-//  Created by Dr. Andreas Plagens on 04.04.25.
-//
-
 import Foundation
 
-print("Hello, World!")
+let maxNumberOfPrimes = 10_000_000
 
+var primes = [2]
+var candidate = 3
+var primesCount = 1
+
+let stopWatch = ContinuousClock()
+
+let duration = stopWatch.measure {
+    while primesCount < maxNumberOfPrimes {
+        let maxDivisor = Int(sqrt(Double(candidate)).rounded())
+        var isPrime = true
+        for primefactor in primes {
+            if primefactor > maxDivisor {
+                break
+            }
+            if candidate % primefactor == 0 {
+                isPrime = false
+                break
+            }
+        }
+
+        if isPrime {
+            primes.append(candidate)
+            primesCount += 1
+        }
+
+        candidate += 2
+    }
+}
+
+print(duration)
