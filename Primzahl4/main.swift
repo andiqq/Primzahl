f845bc9a7115da84caf07cdebf356f99435399ea
Dr. Andreas Plagens <andreas@plagens.eu>
2025-04-04 20:16:30 +0200

    optimized code

--- Primzahl4/main.swift
+++ Primzahl4/main.swift
@@ -1,11 +1,32 @@
-//
-//  main.swift
-//  Primzahl4
-//
-//  Created by Dr. Andreas Plagens on 04.04.25.
-//
-
 import Foundation
 
-print("Hello, World!")
+var primes: [Int] = [2]
+var candidate: Int = 3
+let MAX_NUMBER_OF_PRIMES: Int = 10_000_000
+
+func isPrime(candidate: Int) -> Bool {
+    let maxTeiler = Int(sqrt(Double(candidate)).rounded())
+    for primefactor in primes {
+        if primefactor > maxTeiler {
+            return true
+        }
+        if candidate % primefactor == 0 { return false }
+    }
+    return true
+}
+
+primes.reserveCapacity(MAX_NUMBER_OF_PRIMES)
+
+var timer = Date().timeIntervalSince1970
+
+while primes.count <= MAX_NUMBER_OF_PRIMES {
+    if isPrime(candidate: candidate) { primes.append(candidate) }
+    candidate += 2
+}
+
+timer -= Date().timeIntervalSince1970
+
+//print(primes)
+print(-timer)
+    
 
